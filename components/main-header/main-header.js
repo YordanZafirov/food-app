e7398303efcb17f6e33d7f9c5e588d1bce1ecb39
YordanZafirov <iordanzafirov17@gmail.com>
2024-09-10 16:28:25 +0300

    Refactor main-header component and add NavLink component

--- components/main-header/main-header.js
+++ components/main-header/main-header.js
@@ -4,6 +4,8 @@ import Image from "next/image";
 import logoImg from "@/assets/logo.png";
 import classes from "./main-header.module.css";
 import MainHeaderBackground from "./main-header-background";
+import { usePathname } from "next/navigation";
+import NavLink from "./nav-link";
 
 const MainHeader = () => {
     return (
@@ -18,10 +20,10 @@ const MainHeader = () => {
                 <nav className={classes.nav}>
                     <ul>
                         <li>
-                            <Link href="/meals">Meals</Link>
+                            <NavLink href="/meals">Meals</NavLink>
                         </li>
                         <li>
-                            <Link href="/community">Community</Link>
+                            <NavLink href="/community">Community</NavLink>
                         </li>
                     </ul>
                 </nav>
