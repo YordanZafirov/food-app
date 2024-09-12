f95d7fddede14ab6894e27da5eba9c27efc4e96b
YordanZafirov <iordanzafirov17@gmail.com>
2024-09-12 14:21:13 +0300

    refactor: Added suspense instead of loader page

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,11 +1,17 @@
 import Link from "next/link";
+import { Suspense } from "react";
+
 import classes from "./page.module.css";
 import MealsGrid from "@/components/meals/meals-grid";
 import { getMeals } from "@/lib/meals";
 
-const MealsPage = async () => {
+async function Meals() {
     const meals = await getMeals();
 
+    return <MealsGrid meals={meals} />
+}
+
+const MealsPage = () => {
     return (
         <>
             <header className={classes.header}>
@@ -19,7 +25,9 @@ const MealsPage = async () => {
                 </p>
             </header>
             <main className={classes.main}>
-                <MealsGrid meals={meals} />
+                <Suspense fallback={<p className={classes.loading}>Fetching meals...</p>}>
+                    <Meals />
+                </Suspense>
             </main>
         </>
     );
