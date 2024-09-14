149f49a544c9ae0b7dbc137401cab8b0b5e8e9c9
YordanZafirov <iordanzafirov17@gmail.com>
2024-09-14 21:46:00 +0300

    feat: Added Error component to output message in meals page

---
+++ app/meals/error.js
@@ -0,0 +1,12 @@
+'use client';
+
+const Error = () => {
+    return (
+        <main className="error">
+            <h1>Something went wrong</h1>
+            <p>Try again later</p>
+        </main>
+    );
+}
+
+export default Error;
