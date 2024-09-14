250491b894efcf853705823a90c2b6e13af85158
YordanZafirov <iordanzafirov17@gmail.com>
2024-09-14 22:07:26 +0300

    refactor: Update MealDetailsPage component to display meal details
    - Import Image component from 'next/image'
    - Add styles to page.module.css for the MealDetailsPage component
    - Update MealDetailsPage component to fetch and display meal details from the database

--- app/meals/[mealSlug]/page.js
+++ app/meals/[mealSlug]/page.js
@@ -1,8 +1,28 @@
-const MealDetailsPage = () => {
+import Image from 'next/image';
+
+import classes from './page.module.css';
+import { getMeal } from '@/lib/meals';
+
+const MealDetailsPage = ({ params }) => {
+    const meal = getMeal(params.mealSlug);
     return (
-        <div>
-            <h1>Meal Details Page</h1>
-        </div>
+        <>
+            <header className={classes.header}>
+                <div className={classes.image}>
+                    <Image src={meal.image} alt={meal.title} fill />
+                </div>
+                <div className={classes.headerText}>
+                    <h1>{meal.title}</h1>
+                    <p className={classes.create}>by <a href={`mailto: ${meal.creator_emal}`}>{meal.creator}</a></p>
+                    <p className={classes.summart}>{meal.summary}</p>
+                </div>
+            </header>
+            <main>
+                <p className={classes.instruction} dangerouslySetInnerHTML={{
+                    __html: meal.instructions.replace(/\n/g, '<br>')
+                }}></p>
+            </main>
+        </>
     );
 }
 
