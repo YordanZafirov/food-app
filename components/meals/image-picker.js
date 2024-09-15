96afbb6ed1502adbacc3337e18cd28b6bc5b2a04
YordanZafirov <iordanzafirov17@gmail.com>
2024-09-15 20:02:04 +0300

    refactor: Update ImagePicker component to add image picking functionality

--- components/meals/image-picker.js
+++ components/meals/image-picker.js
@@ -1,16 +1,34 @@
+"use client";
+
+import { useRef } from "react";
 import classes from "./image-picker.module.css";
 
 const ImagePicker = ({ label, name }) => {
+  const imageInput = useRef();
+
+  function pickImageHandler() {
+    imageInput.current.click();
+  }
+
   return (
     <div className={classes.picker}>
       <label htmlFor={name}>{label}</label>
       <div className={classes.controls}>
         <input
+          className={classes.input}
           type="file"
           id={name}
           accept="image/jpg, image/jpeg, image/png"
           name={name}
+          ref={imageInput}
         />
+        <button
+          className={classes.button}
+          type="button"
+          onClick={pickImageHandler}
+        >
+          Pick an image
+        </button>
       </div>
     </div>
   );
