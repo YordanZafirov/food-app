1c5a2da91c7a7244d1ae752ed4636221e3c01899
YordanZafirov <iordanzafirov17@gmail.com>
2024-09-28 17:11:10 +0300

    feat: Add image picking functionality to ImagePicker component

--- components/meals/image-picker.js
+++ components/meals/image-picker.js
@@ -1,19 +1,41 @@
 "use client";
 
-import { useRef } from "react";
+import { useRef, useState } from "react";
 import classes from "./image-picker.module.css";
+import Image from "next/image";
 
 const ImagePicker = ({ label, name }) => {
+  const [pickedImage, setPickedImage] = useState();
   const imageInput = useRef();
 
   function pickImageHandler() {
     imageInput.current.click();
   }
 
+  function handleImageChange(event) {
+    const file = event.target.files[0];
+
+    if (!file) {
+      return;
+    }
+
+    const reader = new FileReader();
+
+    reader.onload = () => {
+      setPickedImage(reader.result);
+    };
+
+    reader.readAsDataURL(file);
+  }
+
   return (
     <div className={classes.picker}>
       <label htmlFor={name}>{label}</label>
       <div className={classes.controls}>
+        <div className={classes.preview}>
+          {!pickedImage && <p>No image picked yet.</p>}
+          {pickedImage && <Image src={pickedImage} alt="Picked image" fill />}
+        </div>
         <input
           className={classes.input}
           type="file"
@@ -21,6 +43,7 @@ const ImagePicker = ({ label, name }) => {
           accept="image/jpg, image/jpeg, image/png"
           name={name}
           ref={imageInput}
+          onChange={handleImageChange}
         />
         <button
           className={classes.button}
